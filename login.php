578e673f175b836c66a3ca6334bc5c956daa20d8
1-8192 <allegranzia@gmail.com>
2019-10-10 11:09:38 -0400

    built log in page

--- login.php
+++ login.php
@@ -1,3 +1,26 @@
+<?php
+    if (isset($_POST['cancel'])) {
+        header('Location: index.php');
+        return;
+    }
+
+    $salt = 'XyZzy12*_';
+    $stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';
+    $message = "";
+
+    if (isset($_POST['email']) && isset($_POST['pass'])) {
+        if (strlen($_POST['email']) < 1 || strlen($_POST['pass']) < 1) {
+            $message = "Email and password and required";
+        } else {
+            if ($stored_hash == hash('md5', $salt.$_POST["pass"])) {
+                header("Location: game.php?name=".urlencode($_POST["email"]));
+            } else {
+                $message = "Incorrect password";
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,6 +28,25 @@
         <?php require_once "bootstrap_styling.php" ?>
     </head>
     <body>
-  
+        <h1>Please Log In</h1>
+        <?php 
+            if ($message !== "") {
+                echo("<h3> $message </h3>");
+            }
+        ?>
+        <form method="POST">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+            <label for="pass">Password</label>
+            <input type="password" name="pass" id="pass">
+            <input type="submit" value="Log In">
+            <input type="submit" name="cancel" value="Cancel">
+            <p>
+            For a password hint, view the source and find a password hint in the HTML comments.
+            <!-- Hint: The password is the three character name of the 
+            programming language used in this class (all lower case) 
+            followed by 123. -->
+            </p>
+        </form>
     </body>
 </html>
