e51712bd73a18b58e1b77cf3e3f690a3b8bfe531
1-8192 <allegranzia@gmail.com>
2019-10-10 11:45:28 -0400

    working on autos page

--- autos.php
+++ autos.php
@@ -1,15 +1,38 @@
 <?php
+    //logging logic
     if (!isset($_GET["name"])) {
         die("Name parameter missing");
     }
+
+    if ( isset($_POST['cancel'])) {
+        header('Location: index.php');
+        return;
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Alessandro Allegranzi - autos db</title>
         <?php require_once "bootstrap_styling.php" ?>
+        <?php require_once "pdo.php" ?>
     </head>
     <body>
-       <h1> Autos Database </h1>
+        <div class="container">
+            <h1> Tracking Autos for <?php htmlentities($_GET["name"]) ?></h1>
+            <form>
+                <p>Make:</p>
+                <input type="text" name="make" id="make">
+                <p>Year:</p>
+                <input type="text" name="year" id="year">
+                <p>Mileage:</p>
+                <input type="text" name="mileage" id="mileage"></br>
+                <input type="submit" value="add">
+                <input type="submit" name="logout" value="Logout">
+            </form>
+       </div>
+       <div>
+        <h2>Automobiles</h2>
+       </div>
     </body>
 </html>
--- pdo.php
+++ pdo.php
@@ -1,4 +1,4 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 'ale', 'pass');
+    $pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 'ale@ale.com', 'php123');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
