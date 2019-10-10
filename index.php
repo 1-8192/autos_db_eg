e7798f0b7d045f927943fc49ab999b76fc317ecc
1-8192 <allegranzia@gmail.com>
2019-10-10 10:52:37 -0400

    made welcome page and created necessary files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Alessandro Allegranzi - autos db</title>
+        <?php require_once "bootstrap_styling.php" ?>
+    </head>
+    <body>
+        <h1>Welcome to the Auto Database</h1>
+        <a href="login.php">Please Log In</a>
+        <p>
+            Attempt to go to <a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+        </p>
+        <a href="https://www.wa4e.com/assn/autosdb/">Specifications for this Application</a>
+    </body>
+</html>
